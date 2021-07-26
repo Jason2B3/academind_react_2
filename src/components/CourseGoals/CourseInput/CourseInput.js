54946a9ad0a6d218d30585bff8cf735c3d15a05e
jason <2B3>
2021-07-26 14:27:29 -0400

    commit 79: CSS Modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
-
+import styles from "./CourseInput.module.css";
 const CourseInput = (props) => {
   // Make the user's input to the form stateful
   const [enteredValue, setEnteredValue] = useState("");
@@ -13,12 +12,15 @@ const CourseInput = (props) => {
   // On submit, run addGoalHandler in App.js & clear fields
   const formSubmitHandler = (event) => {
     event.preventDefault();
+
     // If input is not valid...
     if (enteredValue.trim().length === 0) {
+      console.log("set false input");
       setValidInput(false); // change the input to false, which induces red text
       return;
     }
     // If input is valid...
+    console.log("set valid input");
     setValidInput(true); // change the input to false, which induces red text
     props.onAddGoal(enteredValue); // run addGoal handler
     setEnteredValue(""); // clear fields
@@ -32,7 +34,7 @@ const CourseInput = (props) => {
   //% JSX
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${validInput ? "" : "invalid"}`}>
+      <div className={`${styles.button} ${validInput ? "" : styles.invalid}`}>
         {/*If the input's not valid, add the "invalid" class */}
         <label>Course Goal</label>
         <input
